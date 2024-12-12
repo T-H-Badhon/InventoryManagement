67a97486df6eb0fd39d3923d6d66576e227548b8
T-H-Badhon <thbadhons@gmail.com>
2024-12-12 06:27:29 +0600

    .

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:4000/api/v1",
+    baseUrl: "https://sport-server-main.vercel.app/api/v1",
     credentials: "include",
   }),
   tagTypes: ["category", "product"],
